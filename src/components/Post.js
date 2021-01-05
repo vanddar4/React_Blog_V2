7f21473d2ada80bb7d010534f3157ec6587f0590
vanddar4 <vanddar4@protonmail.com>
2021-01-04 22:58:41 -0500

    messages and update routes

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { QuillDeltaToHtmlConverter } from "quill-delta-to-html";
+import { Link } from "react-router-dom";
 
 const Post = ({post}) => {
   const converter = new QuillDeltaToHtmlConverter(post.content.ops,{});
   const contentHTML = converter.convert();
+  
   return (
     <article className="post container">
       <h1>{post.title}</h1>
+      <p>
+        <Link to={`/edit/${post.slug}`}>Edit</Link>
+      </p>
       <div
         className="content"
         dangerouslySetInnerHTML={{ __html: contentHTML}}
