7f6ca381aef58a0c9f82f0578aacfa1e01e9cdb1
vanddar4 <vanddar4@protonmail.com>
2021-01-05 22:49:32 -0500

    fixing update form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, {useState} from "react";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {
+  BrowserRouter as Router, Switch, Route, Redirect
+} from "react-router-dom";
 import Header from "./components/Header";
 import Posts from "./components/Posts";
 import Post from "./components/Post";
 import PostForm from "./components/PostForm";
 import NotFound from "./components/NotFound";
+import Message from "./components/Message";
 
 import "./App.css";
 
 const App = (props) => {
   const [posts, setPosts] = useState([]);
+  const [message, setMessage] = useState(null);
+
+  const setFlashMessage = (message) => {
+    setMessage(message);
+    setTimeout(() =>{
+      setMessage(null);
+    }, 1600);
+  }
+
+  const getNewSlugFromTitle = (title) =>
+    encodeURIComponent(title.toLowerCase().split(" ").join("-"));
 
   const addNewPost = (post) => {
     post.id = posts.length + 1;
@@ -17,12 +31,23 @@ const App = (props) => {
       post.title.toLowerCase().split(" ").join("-")
     );
     setPosts([...posts, post])
+    setMessage(`saved`);
   };
+
+  const updatePost = (post) => {
+    post.slug = getNewSlugFromTitle(post.title);
+    const index = posts.findIndex((p) => p.id === post.id);
+    const oldPosts = posts.slice(0,index).concat(posts.slice(index + 1));
+    const updatedPosts = [...oldPosts, post].sort((a, b) => a.id - b.id);
+    setPosts(updatedPosts);
+    setFlashMessage(`updated`);
+  }  
 
   return (
     <Router>
       <div className="App">
         <Header />
+        {message && <Message type={message} />}
         <Switch>
           <Route exact path="/" render={() => <Posts posts={posts} />} />
           <Route
@@ -31,14 +56,25 @@ const App = (props) => {
                 const post = posts.find(
                   (post) => post.slug === props.match.params.postSlug
                 );
-                return <Post post={post} />;
+                if (post) {
+                  return <Post post={post} />;
+                } else {
+                  return <Redirect to="/" />
+                }
               }}
           />
-          <Route
-            exact
-            path="/new"
-            render={() => <PostForm addNewPost={addNewPost} />}
-          />
+
+          <Route exact path="/new" render={() => (
+            <PostForm addNewPost={addNewPost} post={{ id: 0, slug: "", title: "", content: ""}} />
+          )}/>
+
+          <Route path="/edit/:postSlug" render={props => {const post = posts.find(post => post.slug === props.match.params.postSlug);
+            if (post){ 
+              return <PostForm updatePost={updatePost} post={post} />;
+            } else {
+              return <Redirect to="/" />;
+            }
+          }}/>
           <Route component={NotFound} />
           </Switch>
         </div>
--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,30 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Redirect } from "react-router-dom";
 import Quill from "react-quill";
 
 import 'react-quill/dist/quill.snow.css';
 
-const PostForm = ({ addNewPost}) => {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+const PostForm = ({ post: propsPost, addNewPost, updatePost}) => {
+  const [post, setPost] = useState({...propsPost});
+  // const [title, setTitle] = useState("");
+  // const [content, setContent] = useState("");
   const [saved, setSaved] = useState("false");
+
+  const prevPostRef = useRef();
+  useEffect(() => {
+    prevPostRef.current = post;
+  }, [post]);
+  const prevPost =prevPostRef.current;
+
+  const quillRef = React.useRef();
+  useEffect(() => {
+    if(prevPost && quillRef.current){
+      if(propsPost.id !== prevPost.id){
+        setPost({...propsPost});
+        quillRef.current.getEditor().setContents(``);
+      }
+    }
+  }, [prevPost, propsPost])
 
   const handlePostForm = (e) => {
     e.preventDefault();
-    if (title) {
-      const post = {
-        title: title,
-        content: content,
-      };
-      addNewPost(post);
+    if (post.title) {
+      if(updatePost) {
+        updatePost(post);
+      } else {
+        addNewPost(post);
+      }
       setSaved(true);
     } else {
       alert("Title Required");
     }
-  }
+  };
+
   if (saved === true) {
     return <Redirect to="/" />;
   }
+
   return (
     <form className="container" onSubmit={handlePostForm}>
       <h1> Add a New Post </h1>
@@ -32,9 +51,15 @@ const PostForm = ({ addNewPost}) => {
           <label htmlFor="form-title">Title:</label>
           <br />
           <input
+            defaultValue={post.title}
             id="form-title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            value={post.title}
+            onChange={(e) => 
+              setPost({ 
+                ...post, 
+                title: e.target.value,
+              })
+            }  
           />
         </p>
         <p>
@@ -42,7 +67,10 @@ const PostForm = ({ addNewPost}) => {
         </p>
         <Quill    
           onChange={(content, delta, source, editor) => {
-            setContent(editor.getContents() );
+            setPost({
+              ...post,
+              content: editor.getContents(),
+            });
           }}
         />
         <p>
